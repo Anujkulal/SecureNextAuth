bbd0fdb168c8bd5a55a8c7020774a709c0685e5c
anujkulal <anujkulal333@gmail.com>
2025-07-19 11:23:19 +0530

    updated tokens.ts on 19-07-2025 11:23:19

--- src/lib/tokens.ts
+++ src/lib/tokens.ts
@@ -1,6 +1,7 @@
 import { getVerificationTokenByEmail } from "@/app/data/verification-token";
 import { v4 as uuidv4 } from "uuid";
 import { db } from "./db";
+import { getPasswordResetTokenByEmail } from "@/app/data/password-reset";
 
 export const generateVerificationToken = async (email: string) => {
   const token = uuidv4();
@@ -24,3 +25,27 @@ export const generateVerificationToken = async (email: string) => {
   });
   return verificationToken;
 };
+
+
+export const generatePasswordResetToken = async (email: string) => {
+  const token = uuidv4();
+    const expires = new Date(new Date().getTime() + 3600 * 1000); // 1 hour from now (add 1 hr), calculates in milliseconds
+const existingToken = await getPasswordResetTokenByEmail(email);
+  if (existingToken) {
+    await db.passwordResetToken.delete({
+      where: {
+        id: existingToken.id,
+      },
+    });
+  }
+
+  const passwordResetToken  = await db.passwordResetToken.create({
+    data: {
+      email,
+      token,
+      expires,
+    }
+  })
+
+  return passwordResetToken;
+}
