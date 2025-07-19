7d12351ec8a564e3fd6836a6e5a37733d67bc8cf
anujkulal <anujkulal333@gmail.com>
2025-07-19 11:23:19 +0530

    updated index.ts on 19-07-2025 11:23:19

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -5,6 +5,14 @@ export const signinSchema = z.object({
     password: z.string().min(3, 'Password must be at least 3 characters'),
 })
 
+export const forgotPasswordSchema = z.object({
+    email: z.email("Invalid email address"),
+})
+
+export const newPasswordSchema = z.object({
+    password: z.string().min(6, 'Password must be at least 6 characters'),
+})
+
 export const signupSchema = z.object({
     name: z.string().min(1, {message: "Name is required"}),
     email: z.email("Invalid email address"),
