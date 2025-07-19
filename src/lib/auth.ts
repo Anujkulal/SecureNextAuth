5846ce30764d23cabe4919c2f435a6f0b341efce
anujkulal <anujkulal333@gmail.com>
2025-07-19 22:26:05 +0530

    updated auth.ts on 19-07-2025 22:26:04

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,8 +2,15 @@ import { auth } from "@/auth";
 
 export const currentUser = async () => {
     const session = await auth();
-    if (!session) {
-        return null;
-    }
-    return session.user;
+    // if (!session) {
+    //     return null;
+    // }
+    return session?.user;
+}
+export const currentRole = async () => {
+    const session = await auth();
+    // if (!session) {
+    //     return null;
+    // }
+    return session?.user?.role;
 }
