8a374971cf78b39d8d86d950427bd37bc43301c3
anujkulal <anujkulal333@gmail.com>
2025-07-19 22:26:04 +0530

    updated page.tsx on 19-07-2025 22:26:04

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -1,20 +1,42 @@
 "use client";
 
 import { signOutAction } from '@/actions/authAction';
+import { settingsAction } from '@/actions/setting';
 import SignoutButton from '@/components/auth/SignoutButton';
 import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
 import { useCurrentUser } from '@/hooks/useCurrentUser';
 import { useSession, signOut } from 'next-auth/react';
-import React from 'react'
+import React, { useTransition } from 'react'
 
 const SettingsPage = () => {    
     // const user = useCurrentUser();
 
+    const {update} = useSession();
+
+    const [isPending, startTransition] = useTransition()
+  const onClick = () => {
+    startTransition(() => {
+      settingsAction({
+        name: "Anuj Kulal",
+      })
+      .then((data) => {
+        update();
+      })
+    })
+  }
     
   return (
-    <div className='p-10 rounded-xl'>
-        <SignoutButton />
-    </div>
+    <Card>
+      <CardHeader>
+        <p>Settings</p>
+      </CardHeader>
+      <CardContent>
+        <Button onClick={onClick} disabled={isPending}>
+          Update name
+        </Button>
+      </CardContent>
+    </Card>
   )
 }
 
