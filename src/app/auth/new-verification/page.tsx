13964b7302515b40a9f26b87ed0bc0ad1e1c8e10
anujkulal <anujkulal333@gmail.com>
2025-07-19 11:23:19 +0530

    updated new-verification on 19-07-2025 11:23:19

---
+++ src/app/auth/new-verification/page.tsx
@@ -0,0 +1,10 @@
+import VerificationForm from '@/components/form/VerificationForm'
+import React from 'react'
+
+const VerificationPage = () => {
+  return (
+    <div><VerificationForm /></div>
+  )
+}
+
+export default VerificationPage
