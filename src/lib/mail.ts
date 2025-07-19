7a14a8db753b5419149a6a0e504d05b6fdc40ad8
anujkulal <anujkulal333@gmail.com>
2025-07-19 13:18:27 +0530

    updated mail.ts on 19-07-2025 13:18:27

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -1,6 +1,21 @@
 import { Resend } from 'resend'
 
 const resend = new Resend(process.env.RESEND_API_KEY || '');
+
+export const sendTwoFactorEmail = async (email: string, token: string) => {
+
+    await resend.emails.send({
+        from: "onboarding@resend.dev",
+        to: email,
+        subject: "Your Two Factor Authentication code",
+        html: `
+        <p>Hi there,</p>
+        <p>Your verification code is: <strong>${token}</strong></p>
+        <p>Please enter this code to complete your login.</p>
+        ` 
+    })
+}
+
 export const sendVerificationEmail = async (email: string, token: string) => {
     const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
 
