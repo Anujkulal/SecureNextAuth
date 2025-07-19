e87cf624eaa833c77de5e653a9504efeb8d223c7
anujkulal <anujkulal333@gmail.com>
2025-07-19 16:50:33 +0530

    updated server on 19-07-2025 16:50:33

---
+++ src/app/(protected)/server/page.tsx
@@ -0,0 +1,13 @@
+import { auth } from '@/auth'
+import React from 'react'
+
+const ServerPage = async () => {
+    const session = await auth();
+  return (
+    <div>
+        {JSON.stringify(session)}
+    </div>
+  )
+}
+
+export default ServerPage
