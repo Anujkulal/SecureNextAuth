d35a464c9d6b106871f168c3c86a1b6af4451afe
anujkulal <anujkulal333@gmail.com>
2025-07-19 13:18:27 +0530

    updated index.ts on 19-07-2025 13:18:27

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -3,6 +3,7 @@ import * as z from 'zod';
 export const signinSchema = z.object({
     email: z.email("Invalid email address"),
     password: z.string().min(3, 'Password must be at least 3 characters'),
+    code: z.optional(z.string()),
 })
 
 export const forgotPasswordSchema = z.object({
