42857f0cb0c6d7e6cf8d2140eb4ea286322c73a0
anujkulal <anujkulal333@gmail.com>
2025-07-19 13:18:27 +0530

    updated tokens.ts on 19-07-2025 13:18:27

--- src/lib/tokens.ts
+++ src/lib/tokens.ts
@@ -1,7 +1,32 @@
+import crypto from "crypto";
 import { getVerificationTokenByEmail } from "@/app/data/verification-token";
 import { v4 as uuidv4 } from "uuid";
 import { db } from "./db";
 import { getPasswordResetTokenByEmail } from "@/app/data/password-reset";
+import { getTwoFactorTokenByEmail } from "@/app/data/two-factor-token";
+
+export const generateTwoFactorToken = async (email: string) => {
+  const token = crypto.randomInt(100_000, 1_000_000).toString();
+  const expires = new Date(new Date().getTime() + 5*60 * 1000); // 5 minutes from now (add 5 min), calculates in milliseconds
+
+  const existingToken = await getTwoFactorTokenByEmail(email);
+  if (existingToken) {
+    await db.twoFactorToken.delete({
+      where: {
+        id: existingToken.id,
+      },
+    })
+  }
+
+  const twoFactorToken = await db.twoFactorToken.create({
+    data: {
+      email,
+      token,
+      expires,
+    }
+  })
+  return twoFactorToken;
+}
 
 export const generateVerificationToken = async (email: string) => {
   const token = uuidv4();
@@ -49,3 +74,4 @@ const existingToken = await getPasswordResetTokenByEmail(email);
 
   return passwordResetToken;
 }
+
