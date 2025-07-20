04964b604bb63da248cafcd260c4e11e2ff3456e
anujkulal <anujkulal333@gmail.com>
2025-07-20 11:16:37 +0530

    updated user-info.tsx on 20-07-2025 11:16:37

--- src/app/(protected)/_components/user-info.tsx
+++ src/app/(protected)/_components/user-info.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { auth } from '@/auth'
+// import { auth } from '@/auth'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
