c2dfdd7bdb6d1ae787239fd914777d5e7d25807b
anujkulal <anujkulal333@gmail.com>
2025-07-20 08:06:43 +0530

    updated layout.tsx on 20-07-2025 08:06:43

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -3,9 +3,15 @@ import Navbar from './_components/navbar'
 
 const ProtectedLayout = ({children}: {children: React.ReactNode}) => {
   return (
-    <div className='h-full w-full flex flex-col gap-y-10 items-center justify-center'>
-        <Navbar />
-        {children}
+    <div className='min-h-screen w-full flex flex-col'>
+        <div className="sticky top-0 z-50 w-full p-4">
+            <Navbar />
+        </div>
+        <main className='flex-1 w-full px-4 pb-8'>
+            <div className="max-w-6xl mx-auto">
+                {children}
+            </div>
+        </main>
     </div>
   )
 }
