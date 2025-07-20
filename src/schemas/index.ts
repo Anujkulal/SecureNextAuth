64cef18db26658ce94a77fe5c05fcb82324dc9c3
anujkulal <anujkulal333@gmail.com>
2025-07-20 08:06:43 +0530

    updated index.ts on 20-07-2025 08:06:43

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,3 +1,4 @@
+import { UserRole } from '@prisma/client';
 import * as z from 'zod';
 
 export const signinSchema = z.object({
@@ -8,7 +9,34 @@ export const signinSchema = z.object({
 
 export const settingsSchema = z.object({
     name: z.optional(z.string()),
+    isTwoFactorEnabled: z.optional(z.boolean()),
+    role: z.enum([UserRole.ADMIN, UserRole.USER]),
+    email: z.optional(z.email()),
+    password: z.optional(z.string().min(6, 'Password must be at least 6 characters')),
+    newPassword: z.optional(z.string().min(6, 'New password must be at least 6 characters')),
 })
+    .refine((data) => {
+       
+        if(data.password && !data.newPassword) {
+            return false; // If password is provided, newPassword must also be provided
+        }
+        return true; // Otherwise, the data is valid
+    }, {
+        message: "New password is required!",
+        path: ["newPassword"],
+
+    })
+    .refine((data) => {
+       
+         if(data.newPassword && !data.password) {
+            return false; // If newPassword is provided, password must also be provided
+        }
+        return true; // Otherwise, the data is valid
+    }, {
+        message: "Password is required!",
+        path: ["password"],
+
+    })
 
 export const forgotPasswordSchema = z.object({
     email: z.email("Invalid email address"),
